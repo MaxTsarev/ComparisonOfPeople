c14de03f97fbdddb6d0192152407b9b84ec38808
MaxTsarev <maxtsar33@gmail.com>
2023-02-08 17:32:54 +0300

    Creating a lambda expression

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -13,8 +14,29 @@ public class Main {
         people.add(new Person("Денис", "Макеев", 50));
         people.add(new Person("Алексей", "Хабиров-Греческий-Корсаков", 80));
         people.add(new Person("Тимур", "Котов Шитов Рогов Лосев", 52));
+        int maxValue = 3;
 
-        Collections.sort(people, new NobilityPrincipleComparator(3));
+
+        Comparator<Person> comparator = (o1, o2) -> {
+            String[] surName1 = o1.getSurname().split(" |-");
+            String[] surName2 = o2.getSurname().split(" |-");
+            if (maxValue > surName1.length & maxValue > surName2.length) {
+                if (surName1.length < surName2.length) {
+                    return -1;
+                }
+                if (surName1.length > surName2.length) {
+                    return 1;
+                }
+            }
+            if (o1.getAge() < o2.getAge()) {
+                return -1;
+            } else if (o1.getAge() > o2.getAge()) {
+                return 1;
+            }
+            return 0;
+        };
+
+        Collections.sort(people, comparator);
         System.out.println(people);
     }
 }
